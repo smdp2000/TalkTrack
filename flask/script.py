d97b1e1e7a787d6cd21d9f0bd0d6b58b4e63bb7b
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2020-07-27 01:33:16 +0530

    Update script.py

--- flask/script.py
+++ flask/script.py
@@ -6,7 +6,7 @@ from pydub import AudioSegment
 import paralleldots
 import json
 import os
-paralleldots.set_api_key("4hIabinTFcXDOCwsykbDyxiQRfoVNdM8r4RGGK1EXVc")
+paralleldots.set_api_key("**4hIabinTFcXDOCwsykbDyxiQRfoVNdM8r4RGGK1EXVc**")
 app = Flask(__name__)
 
 @app.route('/')
